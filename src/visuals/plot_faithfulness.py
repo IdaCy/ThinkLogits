24fe91885c395b138857afa758696eff59144617
IdaCy <ifc00007@students.stir.ac.uk>
2025-04-15 17:13:39 +0000

    visuals done

--- src/visuals/plot_faithfulness.py
+++ src/visuals/plot_faithfulness.py
@@ -1,5 +1,3 @@
-# faithfulness_plot.py
-
 import json
 import numpy as np
 import matplotlib.pyplot as plt
@@ -57,34 +55,34 @@ def plot_faithfulness(
             fractions[i, j] = frac * 100.0  # convert to percentage
 
     x = np.arange(n_hints)
-    #offsets = np.linspace(-bar_width/2, bar_width/2, n_models)
     offsets = np.linspace(-bar_width/2, bar_width/2, n_models) * spacing_factor
 
     fig, ax = plt.subplots(figsize=(8, 5))
 
     for i in range(n_models):
-        # If on matplotlib 3.7+, you can use hatchcolor=hatch_color
-        # If on older versions, omit hatchcolor to avoid errors
+        # Assign color based on i//2, and hatch based on i%2
+        color_for_this_model = bar_colors[i // 2]  # same color for pairs (0 & 1 => color 1, 2 & 3 => color 2)
+        hatch_for_this_model = hatches[i % 2]      # alternate hatch for even/odd index
+
         try:
             bar_container = ax.bar(
                 x + offsets[i],
                 fractions[i],
                 width=bar_width / n_models * n_models,
-                color=bar_colors[i],
+                color=color_for_this_model,
                 edgecolor=frame_color,
-                hatch=hatches[i],
+                hatch=hatch_for_this_model,
                 label=model_names[i],
                 zorder=3,
-                #hatchcolor=hatch_color
             )
         except TypeError:
             bar_container = ax.bar(
                 x + offsets[i],
                 fractions[i],
                 width=bar_width / n_models * n_models,  
-                color=bar_colors[i],
+                color=color_for_this_model,
                 edgecolor=frame_color,
-                hatch=hatches[i],
+                hatch=hatch_for_this_model,
                 label=model_names[i],
                 zorder=3
             )
@@ -106,12 +104,11 @@ def plot_faithfulness(
     
     # x-axis ticks and labels
     ax.set_xticks(x)
-    # Capitalize or nicely format your hint types for display
     display_labels = [h.replace("_", " ").title() for h in hint_types]
     ax.set_xticklabels(display_labels)
     ax.set_xlabel("Hints That The Model Might Use Without Verbalizing Them")
 
-    # Add horizontal grid lines (behind the bars) for style
+    # Add horizontal grid lines (behind the bars)
     ax.grid(True, axis='y', color='gray', linestyle='--', alpha=0.7, zorder=0)
 
     # Put legend in upper right
